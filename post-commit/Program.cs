62383fcb4cad4159eacfb3c6c09334c0f58f6075
Bernd VanSkiver <bvanskiver@reliaslearning.com>
2013-09-10 00:03:57 -0400

    Initial import

---
+++ post-commit/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting;
+using System.Net.Mail;
+using System.Configuration;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.IO;
+
+namespace post_commit
+{
+    delegate void Delegate(string repo, string rev);
+
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            string repo = args[0]; // File system path to repository.
+            string rev = args[1];
+
+            // Get information about commit.
+            string files = ExecuteProcess("svnlook.exe", string.Format("changed -r {0} \"{1}\"", rev, repo)).Trim();
+            string author = ExecuteProcess("svnlook.exe", string.Format("author -r {0} \"{1}\"", rev, repo)).Trim();
+            string log = ExecuteProcess("svnlook.exe", string.Format("log -r {0} \"{1}\"", rev, repo)).Trim();
+
+            // Send a commit email.
+            try
+            {
+                string email = Resources.Changeset_Email;
+
+                email = email.Replace("${revision-url}", string.Format(ConfigurationManager.AppSettings["RevisionUrl"], rev));
+                email = email.Replace("${revision}", rev);
+                email = email.Replace("${author}", author);
+                email = email.Replace("${message}", System.Web.HttpUtility.HtmlEncode(log).Replace("\n", "<br/>"));
+                email = email.Replace("${file-list}", files);
+
+                MailMessage message = new MailMessage(ConfigurationManager.AppSettings["EmailFrom"], ConfigurationManager.AppSettings["EmailTo"]);
+                message.Subject = string.Format("[svn commit {0}:{1}]", Path.GetFileName(repo), rev);
+                message.Body = email;
+                message.IsBodyHtml = true;
+
+                SmtpClient client = new SmtpClient();
+                client.Send(message);
+            }
+            catch (Exception ex)
+            {
+                MailMessage mail = new MailMessage(ConfigurationManager.AppSettings["EmailFrom"], ConfigurationManager.AppSettings["EmailTo"]);
+                mail.Subject = "Post-Commit: " + ex.Message;
+                mail.Body = ex.ToString();
+
+                SmtpClient client = new SmtpClient();
+                client.Send(mail);
+            }
+
+            return 0;
+        }
+
+        static string ExecuteProcess(string fileName, string arguments)
+        {
+            string output;
+
+            // Set up the process.
+            ProcessStartInfo psi = new ProcessStartInfo(fileName, arguments);
+            psi.CreateNoWindow = true;
+            psi.RedirectStandardOutput = true;
+            psi.UseShellExecute = false;
+
+            // Execute the process.
+            using (Process p = Process.Start(psi))
+            {
+                p.WaitForExit(15000);
+
+                // Get the output.
+                output = p.StandardOutput.ReadToEnd();
+            }
+
+            return output;
+        }
+    }
+}
---
+++ pre-commit/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Configuration;
+
+namespace pre_commit
+{
+    class Program
+    {
+        static bool error = false;
+
+        static int Main(string[] args)
+        {
+            string repo = args[0]; // File system path to repository.
+            string txn = args[1];
+
+            try
+            {
+                // Get information about commit.
+                string message = ExecuteProcess("svnlook.exe", string.Format("log -t {0} \"{1}\"", txn, repo)).Trim();
+                string dirs = ExecuteProcess("svnlook.exe", string.Format("dirs-changed -t {0} \"{1}\"", txn, repo)).Trim();
+                string files = ExecuteProcess("svnlook.exe", string.Format("changed -t {0} \"{1}\"", txn, repo)).Trim();
+                string diff = ExecuteProcess("svnlook.exe", string.Format("diff -t {0} \"{1}\"", txn, repo)).Trim();
+
+                // Make sure the message contains some text.
+                if (message.Length == 0)
+                    Error("Commit message is required.");
+
+                // Check for invalid files.
+                Regex regex = new Regex(ConfigurationManager.AppSettings["InvalidFileRegex"], RegexOptions.IgnoreCase | RegexOptions.Multiline);
+                MatchCollection matches = regex.Matches(files.Replace("\r\n", "\n"));
+                foreach (Match match in matches)
+                {
+                    Error(match.Value.Trim().Substring(4) + " not allowed in repository.");
+                }
+
+                // Check for merge conflicts.
+                regex = new Regex(@"^\+(<{7} \.|={7}$|>{7} \.)", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+                if (regex.IsMatch(diff))
+                    Error("Unresolved merge conflict detected.");
+
+                return error ? 1 : 0;
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+                return 1;
+            }
+        }
+
+        static void Error(string message)
+        {
+            Console.Error.WriteLine("** " + message);
+            error = true;
+        }
+
+        static string ExecuteProcess(string fileName, string arguments)
+        {
+            string output;
+
+            // Set up the process.
+            ProcessStartInfo psi = new ProcessStartInfo(fileName, arguments)
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+
+            // Execute the process.
+            using (Process p = Process.Start(psi))
+            {
+                p.WaitForExit(15000);
+
+                // Get the output.
+                output = p.StandardOutput.ReadToEnd();
+            }
+
+            return output;
+        }
+    }
+}
